d009a7f958d6aff2966a8166504c045362968d85
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-11-25 23:19:24 -0300

    Refactor::Se añadio  seguridad de resorte a los módulo, se esat usando BigDecimal para subtotales, SE corregiron el manejo de imágenes de productos y se alinearon los campos de Pago.

--- productos_categorias/src/main/java/com/ms_productos_categorias/service/ProductoService.java
+++ productos_categorias/src/main/java/com/ms_productos_categorias/service/ProductoService.java
@@ -12,6 +12,7 @@ import com.ms_productos_categorias.model.Categoria;
 import com.ms_productos_categorias.model.Producto;
 
 import java.util.List;
+import java.util.Base64; // Importar Base64 esto sirve para convertir la imagen a String
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class ProductoService {
     private final CategoriaRepository categoriaRepository;
 
 
-        // Listar productos filtrando por nombre de categoría
+    // Listar productos filtrando por nombre de categoría
     public List<ProductoDTO> listarPorCategoria(String nombreCategoria) {
         return productoRepository.findByCategoriaNombre(nombreCategoria)
                 .stream()
@@ -39,7 +40,10 @@ public class ProductoService {
 
     //Crear categoría (recibiendo un ProductoDTO)
     public ProductoDTO crear(ProductoDTO dto) {
-
+        byte[] imagenBytes = null;
+        if (dto.getImagenBase64()!= null) {
+            imagenBytes = Base64.getDecoder().decode(dto.getImagenBase64());
+        }
         Categoria categoria = categoriaRepository.findByNombre(dto.getCategoria())
                 .orElseThrow(() -> new IllegalArgumentException("Categoría no encontrada"));
         
@@ -50,29 +54,33 @@ public class ProductoService {
                 .categoria(categoria)
                 .stock(dto.getStock())
                 .estado(dto.getEstado())
+                .imagen(imagenBytes)
                 .build();
         
         Producto guardado = productoRepository.save(producto);
-
         return toDTO(guardado);
     }
 
 
     // Convertir de model a DTO
     private ProductoDTO toDTO(Producto producto) {
-    return ProductoDTO.builder()
-            .id(producto.getId())
-            .nombre(producto.getNombre())
-            .descripcion(producto.getDescripcion())
-            .precio(producto.getPrecio())
-            .categoria(producto.getCategoria().getNombre())
-            .stock(producto.getStock())
-            .estado(producto.getEstado())
-            .disponibilidad(calcularDisponibilidad(producto.getStock()))
-            .sku(producto.getSku())
-            .build();
-}
-
+        String  imagenBase64 = null;
+        if (producto.getImagen() != null) {
+            imagenBase64 = Base64.getEncoder().encodeToString(producto.getImagen());
+        }
+        return ProductoDTO.builder()
+                .id(producto.getId())
+                .nombre(producto.getNombre())
+                .descripcion(producto.getDescripcion())
+                .precio(producto.getPrecio())
+                .categoria(producto.getCategoria().getNombre())
+                .stock(producto.getStock())
+                .estado(producto.getEstado())
+                .disponibilidad(calcularDisponibilidad(producto.getStock()))
+                .sku(producto.getSku())
+                .imagenBase64(imagenBase64)
+                .build();
+    }
 
     //Logica de disponibilidad basada en el stock
     private String calcularDisponibilidad(Integer stock) {
@@ -93,7 +101,6 @@ public class ProductoService {
 
     //Actualizar producto por ID
     public ProductoDTO actualizar(Long id, ProductoDTO dto) {
-        
         Producto producto = productoRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Producto no encontrado"));
 
@@ -106,6 +113,10 @@ public class ProductoService {
         producto.setCategoria(categoria);
         producto.setStock(dto.getStock());
         producto.setEstado(dto.getEstado());
+
+        if (dto.getImagenBase64() != null) {
+            producto.setImagen(Base64.getDecoder().decode(dto.getImagenBase64()));
+        }
 
         Producto actualizado = productoRepository.save(producto);
 
