8446c0b5c8611bd3eed0bc648d39ecb042b82662
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-11-25 23:08:17 -0300

    Feat : cambios minimos

--- auth_usuarios/src/main/java/com/ms_auth_usuarios/model/Usuario.java
+++ auth_usuarios/src/main/java/com/ms_auth_usuarios/model/Usuario.java
@@ -14,49 +14,40 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Usuario {
 
-
-    // Atributos de la entidad Usuario
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_usuario")
     private Long id;
 
-    @Column(nullable = false, length = 80)
+    @Column(nullable = false, length = 100)
     private String nombre;
 
-    @Column(nullable = false, length = 80)
+    @Column(nullable = false, length = 100)
     private String apellido;
 
     @Column(nullable = false, unique = true, length = 12)
     private String rut;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    @Column(nullable = false, length = 15)
+    @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, length = 100)
-    private String direccion;
-
-    @Column(nullable = false, length = 11)
+    @Column(nullable = false, length = 15)
     private String telefono;
 
+    @Column(nullable = false, length = 200)
+    private String direccion;
 
-    //Enum para definir el rol = Administrador o Usuario (Cliente)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    private Rol rol;
+    private Rol rol; // CLIENTE / ADMIN
 
-    // Fecha de registro del usuario
-    @Column(name = "fecha_registro", nullable = false)
-    private LocalDateTime fechaRegistro;
-    
-
-    // Estado del usuario (Activo, Inactivo, Suspendido, etc.)
     @Column(nullable = false, length = 20)
-    private String estado;
+    private String estado; // "Activo", "Inactivo"
+
+    private LocalDateTime fechaRegistro;
 }
+
