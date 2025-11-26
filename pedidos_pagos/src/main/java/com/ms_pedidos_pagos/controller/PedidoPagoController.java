09762452c33efdbde19f3a69dfec0f27b3900bed
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-11-26 20:45:07 -0300

    Feat: Nuevo get para admin y usuarios

--- pedidos_pagos/src/main/java/com/ms_pedidos_pagos/controller/PedidoPagoController.java
+++ pedidos_pagos/src/main/java/com/ms_pedidos_pagos/controller/PedidoPagoController.java
@@ -2,9 +2,12 @@ package com.ms_pedidos_pagos.controller;
 
 import com.ms_pedidos_pagos.dto.ComprobantePagoDTO;
 import com.ms_pedidos_pagos.dto.CrearPedidoPagoDTO;
+import com.ms_pedidos_pagos.model.Pedido;
 import com.ms_pedidos_pagos.service.PedidoPagoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.ms_pedidos_pagos.repository.PedidoRepository;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -13,10 +16,22 @@ import org.springframework.web.bind.annotation.*;
 public class PedidoPagoController {
 
     private final PedidoPagoService pedidoPagoService;
+    private final PedidoRepository pedidoRepository;
 
     // POST /pedidos/pagar
     @PostMapping("/pagar")
     public ComprobantePagoDTO pagar(@RequestBody CrearPedidoPagoDTO dto) {
         return pedidoPagoService.crearPedidoYPago(dto);
+    }
+
+    // GET listado general (Admin / Vendedor)
+    @GetMapping
+    public List<Pedido> listarTodos() {
+        return pedidoRepository.findAll();
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public List<Pedido> listarPorUsuario(@PathVariable String usuarioId) {
+        return pedidoRepository.findByUsuarioId(usuarioId);
     }
 }
--- pedidos_pagos/src/main/java/com/ms_pedidos_pagos/repository/PedidoRepository.java
+++ pedidos_pagos/src/main/java/com/ms_pedidos_pagos/repository/PedidoRepository.java
@@ -2,6 +2,9 @@ package com.ms_pedidos_pagos.repository;
 
 import com.ms_pedidos_pagos.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<Pedido, String> {
+    List<Pedido> findByUsuarioId(String usuarioId);
+
 }
