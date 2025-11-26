528601a2946f76970de0522fb17e7f374cad5ac6
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-11-25 23:28:41 -0300

    Feat: Con esto Admin y Vendedor quedan registrados al lanzar el ms

---
+++ auth_usuarios/src/main/java/com/ms_auth_usuarios/config/DataInitializer.java
@@ -0,0 +1,56 @@
+package com.ms_auth_usuarios.config;
+
+import com.ms_auth_usuarios.model.Rol;
+import com.ms_auth_usuarios.model.Usuario;
+import com.ms_auth_usuarios.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (!usuarioRepository.existsByEmail("admin@techhive.cl")) {
+            Usuario admin = Usuario.builder()
+                    .nombre("Admin")
+                    .apellido("Principal")
+                    .rut("11111111-1")
+                    .email("admin@techhive.cl")
+                    .password(passwordEncoder.encode("Admin12345*"))
+                    .telefono("999999999")
+                    .direccion("Sede DUOC")
+                    .rol(Rol.ADMIN)
+                    .estado("Activo")
+                    .fechaRegistro(LocalDateTime.now())
+                    .build();
+            usuarioRepository.save(admin);
+        }
+
+        if (!usuarioRepository.existsByEmail("vendedor@techhive.cl")) {
+            Usuario vendedor = Usuario.builder()
+                    .nombre("Vendedor")
+                    .apellido("Tienda")
+                    .rut("22222222-2")
+                    .email("vendedor@techhive.cl")
+                    .password(passwordEncoder.encode("Vendedor123*"))
+                    .telefono("888888888")
+                    .direccion("Sucursal Local")
+                    .rol(Rol.VENDEDOR)
+                    .estado("Activo")
+                    .fechaRegistro(LocalDateTime.now())
+                    .build();
+            usuarioRepository.save(vendedor);
+        }
+    }
+}
+
