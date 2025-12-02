2deca4bc0d14b6da41f4007ff1b34a7542b44abd
Samu-Duoc <sam.fuenzalida@duocuc.cl>
2025-12-02 15:02:07 -0300

    Cambio al registro

--- auth_usuarios/src/main/java/com/ms_auth_usuarios/dto/RegistroUsuarioDTO.java
+++ auth_usuarios/src/main/java/com/ms_auth_usuarios/dto/RegistroUsuarioDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import jakarta.validation.constraints.Pattern;
 
 @Data
 public class RegistroUsuarioDTO {
@@ -22,7 +23,8 @@ public class RegistroUsuarioDTO {
     private String email;
     
     @NotBlank
-    @Size(min = 10, message = "La contraseña debe tener al menos 10 caracteres")
+    @Size(min = 6, max = 8, message = "La contraseña debe tener entre 6 y 8 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[+*]).{6,8}$",message = "La contraseña debe contener al menos una mayúscula y un carácter especial (+ o *)")
     private String password;
 
     @NotBlank
